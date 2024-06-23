b9f941d0553c26d78a2349c2453c4a94762fe585
Hector restrepo <hector.soto20034@gmail.com>
2024-06-23 09:30:23 -0500

    agregacion del nuevo Blog

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import DisenoLogin from './components/login';
+
+
+function App() {
+
+  return (
+    <>
+      <DisenoLogin></DisenoLogin>
+    </>
+  )
+}
+
+export default App
