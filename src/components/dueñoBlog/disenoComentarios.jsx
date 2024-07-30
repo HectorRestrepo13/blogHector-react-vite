5ec465529101276382c1fd16d7ae524dde0a8d11
Hector restrepo <hector.soto20034@gmail.com>
2024-07-30 18:08:13 -0500

    terminado la funcionalidad de editar comentarios

--- src/components/dueñoBlog/disenoComentarios.jsx
+++ src/components/dueñoBlog/disenoComentarios.jsx
@@ -1,5 +1,5 @@
 import './css/styleEntradas.css'
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Swal from 'sweetalert2';
 import bootstrap from 'bootstrap/dist/js/bootstrap.bundle'
 
@@ -9,7 +9,15 @@ const DisenoComentarios = ({ idEntrada, func_mostrarComentarios }) => {
 
     let [jsonComentarios, setJsonComentarios] = useState(null);
     let [insertacionComentario, setInsertacionComentario] = useState(null)
+    let [editarComentario, setEditarComentario] = useState(null)
     const [error, setError] = useState(null);
+
+    // aca asigno los DOM con el "useRef"
+    const nombreUsuarioComentarioRef = useRef(null);
+    const inputNuevoComentarioRef = useRef(null);
+    const idComentarioRef = useRef(null);
+    const modalRef = useRef(null);
+
 
     //  FUNCION DONDE VOY A CONSUMIR LA API PARA TRAER LOS DATOS DE LOS COMENTARIOS
     const func_traerDatosComentarios = async () => {
@@ -157,20 +165,145 @@ const DisenoComentarios = ({ idEntrada, func_mostrarComentarios }) => {
 
     // FUNCION PARA EDITAR EL COMENTARIO
 
-    const func_editarComentario = (id, nombre, apellido) => {
+    const func_editarComentario = (id, nombre, apellido, DescripcionComentario) => {
         // enviamos los datos antes de abrir el modal
-        document.getElementById("nombreUsuarioComentario").textContent = `${nombre} ${apellido}`;
-        document.getElementById("inputNuevoComentario").value = "Hola";
+        nombreUsuarioComentarioRef.current.textContent = `${nombre} ${apellido}`;
+        inputNuevoComentarioRef.current.value = DescripcionComentario;
+        idComentarioRef.current.value = id;
 
-
-        var myModal = new bootstrap.Modal(document.getElementById('modalEditar'));
+        var myModal = new bootstrap.Modal(modalRef.current);
         myModal.show();
     }
 
     // -- FIN FUNCION --
 
+    // FUNCION DONDE AL DARLE CLIC EN GUARDAR EN EL BOTON DEL MODAL SE CONSUMIRA LA API DE UPDATE
+
+    const func_guardarEdicionComentario = () => {
+
+        try {
+
+            Swal.fire({
+                title: "Esta Seguro de Editar El comentario?",
+                text: "Se Editara el Comentario y ya no habra vuelta atras!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes!"
+            }).then(async (result) => {
+                if (result.isConfirmed) {
+                    let datosLocalStore = window.localStorage;
+                    let cedulaUsuario = JSON.parse(datosLocalStore.getItem("usuario"));
+                    cedulaUsuario = cedulaUsuario.descripcion.CedulaUsuario
+                    let datos = {
+                        DescripcionComentario: inputNuevoComentarioRef.current.value,
+                        UsuarioCedulaUsuario: cedulaUsuario,
+                        idComentario: idComentarioRef.current.value
+                    }
+                    setEditarComentario("cargando")
+                    let urlEditarComentario = await fetch(`https://proyectoblog.onrender.com/comentarios/editarComentario/`,
+                        {
+                            method: "PUT",
+                            headers: {
+                                "Content-Type": "application/json", // Establece el tipo de contenido en JSON
+                            },
+                            body: JSON.stringify(datos)
+
+                        }
+                    )
+
+                    if (!urlEditarComentario.ok) {
+                        throw new Error(`Hubo un error al Consumir la URL para editar el comentario: ${urlEditarComentario.status}`)
+                    }
+
+                    let jsonComentarioInsertado = await urlEditarComentario.json();
+
+                    if (jsonComentarioInsertado.status == true) {
+                        Swal.fire({
+                            title: "Se Inserto con exito!!",
+                            text: "Comentario Actualizado",
+                            icon: "success"
+                        }).then(() => {
+                            // Obtener la instancia existente del modal
+                            var myModalInstance = bootstrap.Modal.getInstance(modalRef.current);
+                            if (myModalInstance) {
+                                myModalInstance.hide();
+                                setEditarComentario(null);
+                                func_traerDatosComentarios();
+
+                            }
+
+                        })
+                    }
+                    else {
+                        console.log(jsonComentarioInsertado)
+                        Swal.fire({
+                            title: "Hubo un Error",
+                            text: "verificar la Consola",
+                            icon: "warning"
+                        });
+                    }
 
 
+                }
+            });
+
+        } catch (error) {
+            console.log(`ERROR ${error}`)
+
+            console.log(jsonComentarioInsertado)
+            Swal.fire({
+                title: "Hubo un Error",
+                text: "verificar la Consola",
+                icon: "error"
+            });
+        }
+
+
+    }
+
+    // -- FIN FUNCION --
+
+
+    // FUNCION PARA ELIMINAR EL COMENTARIO
+    const func_eliminarComentario = (idComentario) => {
+        const swalWithBootstrapButtons = Swal.mixin({
+            customClass: {
+                confirmButton: "btn btn-success",
+                cancelButton: "btn btn-danger"
+            },
+            buttonsStyling: false
+        });
+        swalWithBootstrapButtons.fire({
+            title: "Estas Seguro de Eliminar Este Comentario?",
+            text: "Ya no Habra vuelta Atras!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonText: "Si, Eliminar!",
+            cancelButtonText: "No, Cancelar!",
+            reverseButtons: true
+        }).then((result) => {
+            if (result.isConfirmed) {
+                swalWithBootstrapButtons.fire({
+                    title: "Eliminado!",
+                    text: "Comentario Eliminado con Exito.",
+                    icon: "success"
+                });
+            } else if (
+                /* Read more about handling dismissals below */
+                result.dismiss === Swal.DismissReason.cancel
+            ) {
+                swalWithBootstrapButtons.fire({
+                    title: "Cancelado",
+                    text: "Se Cancelo la eliminada del Comentario :)",
+                    icon: "error"
+                });
+            }
+        });
+    }
+
+    // --  FIN FUNCION --
 
 
 
@@ -372,13 +505,13 @@ const DisenoComentarios = ({ idEntrada, func_mostrarComentarios }) => {
                                                         <h5 className="card-title">{datos.Usuario.NombreUsuario}</h5>
                                                     </div>
                                                     <div className="contenedorBTNEditar">
-                                                        <svg onClick={func_editarComentario(datos.id, datos.Usuario.NombreUsuario, datos.Usuario.ApellidoUsuario)} xmlns="http://www.w3.org/2000/svg" style={{ marginRight: "10px" }} width="28" height="40" fill="currentColor" className="bi bi-pencil" viewBox="0 0 16 16">
+                                                        <svg onClick={() => func_editarComentario(datos.id, datos.Usuario.NombreUsuario, datos.Usuario.ApellidoUsuario, datos.DescripcionComentario)} xmlns="http://www.w3.org/2000/svg" style={{ marginRight: "10px" }} width="28" height="40" fill="currentColor" className="bi bi-pencil" viewBox="0 0 16 16">
                                                             <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" />
                                                         </svg>
                                                     </div>
                                                     <div className="contenedorBTNBorrar">
 
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="40" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
+                                                        <svg onClick={() => func_eliminarComentario(datos.id)} xmlns="http://www.w3.org/2000/svg" width="28" height="40" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
                                                             <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
                                                             <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
                                                         </svg>
@@ -456,20 +589,33 @@ const DisenoComentarios = ({ idEntrada, func_mostrarComentarios }) => {
                                 {/*  ACA VA IR EL DISEÑO DEL MODAL DE EDITAR EL COMENTARIO */}
                                 <div className="row">
                                     <div className="col">
-                                        <div className="modal fade" id="modalEditar" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                        <div ref={modalRef} className="modal fade" id="modalEditar" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                             <div className="modal-dialog modal-dialog-centered">
                                                 <div className="modal-content disenoModal">
                                                     <div className="modal-header">
-                                                        <h1 className="modal-title fs-5" id="nombreUsuarioComentario">Hector Restrepo</h1>
+                                                        <input ref={idComentarioRef} hidden type="text" />
+                                                        <h1 ref={nombreUsuarioComentarioRef} className="modal-title fs-5" id="nombreUsuarioComentario">Hector Restrepo</h1>
                                                         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                     </div>
                                                     <div className="modal-body">
-                                                        <textarea className="cometarioNuevo" id="inputNuevoComentario" rows="5"></textarea>
+                                                        <textarea ref={inputNuevoComentarioRef} className="cometarioNuevo" id="inputNuevoComentario" rows="5"></textarea>
 
                                                     </div>
                                                     <div className="modal-footer">
-                                                        <button type="button" className="btnCancelarComentario" data-bs-dismiss="modal">Cancelar</button>
-                                                        <button type="button" className="btnGuardarCometario">Guardar</button>
+                                                        {
+                                                            editarComentario == null ? (<><button type="button" className="btnCancelarComentario" data-bs-dismiss="modal">Cancelar</button>
+                                                                <button type="button" onClick={func_guardarEdicionComentario} className="btnGuardarCometario">Guardar</button> </>) : (<>
+                                                                    <div className="spinner-grow text-warning" role="status">
+                                                                        <span className="visually-hidden">Loading...</span>
+                                                                    </div>
+                                                                    <div className="spinner-grow text-info" role="status">
+                                                                        <span className="visually-hidden">Loading...</span>
+                                                                    </div>
+                                                                    <div className="spinner-grow text-light" role="status">
+                                                                        <span className="visually-hidden">Loading...</span>
+                                                                    </div></>)
+                                                        }
+
                                                     </div>
                                                 </div>
                                             </div>
